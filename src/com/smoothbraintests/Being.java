bec509d9959d46a129911ffae45576d422cc01f1
Ruairidh Barnes <ruairidhb@gmail.com>
2020-05-21 00:16:56 +0100

    adding some basic tests. the whole thing doesn't work but it doesn't matter because i'm having fun

---
+++ src/com/smoothbraintests/Being.java
@@ -0,0 +1,65 @@
+package com.smoothbraintests;
+
+import com.smoothbrain.Brain;
+
+import java.util.List;
+
+public class Being implements Comparable<Being> {
+    private int fitness;
+    private Brain brain;
+
+    public Being(int inputCount, int outputCount, int layerCount, int layerHeight){
+        this.brain = new Brain(inputCount, outputCount, layerCount, layerHeight);
+    }
+
+    public Being(Brain brain){
+        this.brain = brain;
+    }
+
+    public void resetFitness(){
+        this.fitness = 0;
+    }
+
+    public void mutate(float mutationRate){
+        this.brain.mutate(mutationRate);
+    }
+
+    public int think(List<Float> inputs){
+        brain.setInputs(inputs);
+        List<Float> thoughts = brain.think();
+
+        if(thoughts.get(0) > 1f && thoughts.get(0) < 100f){
+            this.fitness += 1000;
+        }
+
+        if(thoughts.get(1) > 100f && thoughts.get(1) < 140f){
+            this.fitness += 10;
+        }
+
+        if(thoughts.get(2) > 1f && thoughts.get(2) < 500f){
+            this.fitness += 10;
+        }
+
+        if(thoughts.get(3) < -0.0f && thoughts.get(3) > -1000.0f){
+            this.fitness += 100000;
+        }
+
+
+        return this.fitness;
+    }
+
+    public Brain getBrain(){
+        return this.brain;
+    }
+
+    public int getFitness(){
+        return fitness;
+    }
+
+    @Override
+    public int compareTo(Being o) {
+        return this.fitness - o.getFitness();
+    }
+
+
+}
--- src/com/smoothbraintests/Main.java
+++ src/com/smoothbraintests/Main.java
@@ -1,4 +1,66 @@
 package com.smoothbraintests;
 
+import com.smoothbrain.Brain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+        ArrayList<Being> currentGeneration = new ArrayList<Being>();
+        ArrayList<Being> nextGeneration = new ArrayList<Being>();
+        for(int i = 0; i < 100; i++){
+            currentGeneration.add(new Being(10,4,4,4));
+        }
+
+        ArrayList<Float> inputs = new ArrayList<Float>();
+
+        inputs.add(1.0f);
+        inputs.add(2.0f);
+        inputs.add(3.0f);
+        inputs.add(4.0f);
+        inputs.add(4.0f);
+        inputs.add(4.0f);
+        inputs.add(4.0f);
+        inputs.add(4.0f);
+        inputs.add(4.0f);
+        inputs.add(4.0f);
+
+        int generation = 0;
+        int sumFitness = 0;
+        while(true){
+            generation++;
+
+            sumFitness = 0;
+
+            for(Being b : currentGeneration){
+                sumFitness += b.think(inputs);
+            }
+
+            Collections.reverse(currentGeneration);
+
+            System.out.println("Generation " + generation + " total fitness: " + sumFitness + " average fitness: " + (sumFitness / 100) + " best fitness: " + currentGeneration.get(0).getFitness());
+
+
+
+            for(int i = 0; i < 10; i++){
+                currentGeneration.get(i).mutate(0.0000001f);
+                currentGeneration.get(i).resetFitness();
+                nextGeneration.add(currentGeneration.get(i));
+            }
+
+            currentGeneration.clear();
+
+            for(int i = 0; i < 10; i++){
+                currentGeneration.addAll(nextGeneration);
+            }
+
+            nextGeneration.clear();
+        }
+
+    }
+
 }
